93256cc0ebb158f0543197b7ba5ca8a23c1e77c9
Shengli Liu <liushlcn@gmail.com>
2021-08-15 15:33:42 +0800

    Update simple.r

--- simple.r
+++ simple.r
@@ -1,3 +1,10 @@
  ## Sort a named list in R
  ## https://stackoverflow.com/questions/27312311/sort-a-named-list-in-r
  lst = lst[order(names(lst))]
+
+# remove the axis text with default barplot
+ barplot(col = pals::tableau20(6), las = 1, xlim = c(0, 35), border = F, xpd = T, ann = F, xaxt = 'n', horiz  = T, beside = T)
+
+# change the axis text and its angle
+# https://www.tenderisthebyte.com/blog/2019/04/25/rotating-axis-labels-in-r/
+# https://stats.idre.ucla.edu/r/faq/how-can-i-change-the-angle-of-the-value-labels-on-my-axes/
