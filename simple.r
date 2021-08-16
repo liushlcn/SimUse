6631dc8ad8c8ca850c50f0918254f28caf9acad3
sanrenyoufang <liushlcn@gmail.com>
2021-08-16 16:00:26 +0800

    changes

--- simple.r
+++ simple.r
@@ -18,3 +18,19 @@
 # https://stackoverflow.com/questions/4863341/fast-vectorized-merge-of-list-of-data-frames-by-row?rq=1
 # Create a column based on the name of the element list that contain the data frame in R
 # https://stackoverflow.com/questions/48255351/create-a-column-based-on-the-name-of-the-element-list-that-contain-the-data-fram
+
+library(tidyverse)
+
+df <- data.frame(sample = letters[1:6],
+  value = c(1,1.5,NA,3,4, 2)) %>% 
+  mutate(cut_value = cut(value, breaks = c(1,2,3,4)),
+    color = factor(cut_value, 
+      levels = levels(cut_value),
+      labels = c('darkred', 'orange', 'yellow')),
+    color = fct_explicit_na(color, na_level = 'grey85'))
+ggplot(df, aes(sample, value))+
+  geom_bar(stat = 'identity', aes(fill = color))+
+  scale_fill_identity(guide = 'legend',
+    labels = levels(df$cut_value),
+    breaks = levels(df$color)[-4])
+
